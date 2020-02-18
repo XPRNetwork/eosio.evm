26c7f2c0c1263871a9b0870108302c5412990b4e
jafri <syed.jafri2@ucalgary.ca>
2020-02-17 19:27:48 -0700

    rm cached files

--- tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jafri/eosio.evm/tests
-# Build directory: /Users/jafri/eosio.evm/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(eosio_evm_unit_test "/Users/jafri/eosio.evm/tests/unit_test" "--run_test=eosio_evm_tests" "--report_level=detailed" "--color_output")
-set_tests_properties(eosio_evm_unit_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/jafri/eosio.evm/tests/CMakeLists.txt;40;add_test;/Users/jafri/eosio.evm/tests/CMakeLists.txt;0;")

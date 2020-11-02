bb522372f550931b165165aeedb9db09577ddc4c
Eugene Luzgin <eugene.luzgin@mx.com>
2020-11-02 06:23:42 -0700

    Remove eos-evm-js

--- eos-evm-js/test/debugging.test.ts
+++
@@ -1,8 +0,0 @@
-import { EosEvmApi } from '../src/eos-evm-js'
-import { allowanceAddress, api, sender, ethContractAddress } from './common'
-
-describe('Debug Test', () => {
-  it(`transfer from`, async () => {
-    console.log(await api.eos.getEthAccountByEosAccount('vestvestvest'))
-  })
-})
